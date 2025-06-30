bb7757e756d90ba22e4de7d04bb9b7c8358ca163
Idigo Luwum <luwum@pm.me>
2025-06-29 22:59:37 -0500

    chore: update npm configuration for pnpm.

--- .config/p3/40-node.sh
+++ .config/p3/40-node.sh
@@ -11,9 +11,6 @@ fi
 if [ ! -d "${XDG_CACHE_HOME}/npm" ]; then
 	mkdir "${XDG_CACHE_HOME}/npm"
 fi
-var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
-var "NVM_LAZY_LOAD" "true"
 var "NPM_CONFIG_USERCONFIG" "${XDG_CONFIG_HOME}/npm/npmrc"
 var "NPM_CONFIG_INIT_MODULE" "${XDG_DATA_HOME}/npm/npm-init.js"
 var "NPM_CONFIG_CACHE" "${XDG_CACHE_HOME}/npm"
-var "PATH" "$XDG_BIN_HOME/yarn/bin:$PATH"
