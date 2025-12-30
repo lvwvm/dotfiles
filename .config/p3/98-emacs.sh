4482efdb2f269dbd9c1792f87ebc7ac1fecda8a4
Idigo Luwum <luwum@pm.me>
2025-12-29 21:18:38 -0600

    refactor(doom): Checmacs2 might not even be necessary.

--- .config/p3/98-emacs.sh
+++ .config/p3/98-emacs.sh
@@ -11,47 +11,48 @@ var "EMACSEN_DATA_HOME" "${XDG_DATA_HOME}/emacsen"
 
 # This script was created, primarily, to handle the chemacs2 emacs profile manager.
 # Chemacs2 MUST exist at ~/.emacs.d
-if [ ! -d "$XDG_DATA_HOME/chemacs2" ]; then
-  git clone https://github.com/plexus/chemacs2.git "$XDG_DATA_HOME/chemacs2"
-fi
+# if [ ! -d "$XDG_DATA_HOME/chemacs2" ]; then
+#  git clone https://github.com/plexus/chemacs2.git "$XDG_DATA_HOME/chemacs2"
+# fi
 
 # The "emacsen" folder is storing emacs.d "distributions" like spacemacs, doom, prelude etc.
 [ ! -d "$EMACSEN_DATA_HOME" ] && mkdir "$EMACSEN_DATA_HOME"
+[ ! -d "$EMACSEN_CONFIG_HOME" ] && mkdir "$EMACSEN_CONFIG_HOME"
 
 # Emacs init search file order ~/.emacs.el, ~/.emacs, ~/.emacs.d/init.el - but remeber chemacs must see ~/.emacs.d
 # These path are prefered by emacs for backwards compatibility.
 # Setting XDG_CONFIG_HOME alters emacs' search behavior, although the aforementioned files must be deleted, renamed, and/or unlinked in order to do so.
-for _file in ~/.emacs.d/init.el ~/.emacs.el ~/.emacs; do
-  case $_file in
-    ~/.emacs.d/init.el)
-      _path=$(dirname $_file)
+# for _file in ~/.emacs.d/init.el ~/.emacs.el ~/.emacs; do
+#  case $_file in
+#    ~/.emacs.d/init.el)
+#      _path=$(dirname $_file)
+#
+#      # no existing ~/.emacs.d or link.
+#      if [ ! -d "$_path" ] && [ ! -h "$_path" ]; then
+#        ln -sT "$_path" "$XDG_DATA_HOME/chemacs2"
+#
+#      # existing ~/.emacs.d, no link
+#      elif [ -d "$_path" ] && [ ! -h "$_path" ]; then
+#        # move emacs.d contents to emacsen folder.
+#       mv --no-clobber \
+#         --backup=simple \
+#         --suffix=.bk \
+#         "$_path" "$EMACSEN_DATA_HOME"
+#       ln -sT "$_path" "$XDG_DATA_HOME/chemacs2"
+#     fi
+#     ;;
 
-      # no existing ~/.emacs.d or link.
-      if [ ! -d "$_path" ] && [ ! -h "$_path" ]; then
-        ln -sT "$_path" "$XDG_DATA_HOME/chemacs2"
-
-      # existing ~/.emacs.d, no link
-      elif [ -d "$_path" ] && [ ! -h "$_path" ]; then
-        # move emacs.d contents to emacsen folder.
-        mv --no-clobber \
-          --backup=simple \
-          --suffix=.bk \
-          "$_path" "$EMACSEN_DATA_HOME"
-        ln -sT "$_path" "$XDG_DATA_HOME/chemacs2"
-      fi
-      ;;
-
-    ~/.emacs.el | ~/.emacs)
-      # no exist emacs init file or link
-      if [ -f "$_file" ] && [ ! -h "$_file" ]; then
-        mv --backup=simple \
-          --suffix=.bk \
-          "$_file" "$EMACSEN_DATA_HOME/emacs.d/"
-      elif [ -f "$_file" ] && [ -h "$_file" ]; then
-        unlink "$_file"
-      fi
-      ;;
-  esac
-  unset _file
-  unset _path
-done
+#   ~/.emacs.el | ~/.emacs)
+#     # no exist emacs init file or link
+#     if [ -f "$_file" ] && [ ! -h "$_file" ]; then
+#       mv --backup=simple \
+#         --suffix=.bk \
+#         "$_file" "$EMACSEN_DATA_HOME/emacs.d/"
+#     elif [ -f "$_file" ] && [ -h "$_file" ]; then
+#       unlink "$_file"
+#     fi
+#     ;;
+# esac
+# unset _file
+# unset _path
+# done
