27c579a04dfd347405375d770671d930eea655cd
Idigo Luwum <luwum@pm.me>
2026-06-11 15:57:37 -0500

    feat(login): track bash dotfiles and X11 session entry points
    bash parity with zsh login flow; .xinitrc/.Xclients/.screenlayout
    restore graphical session and monitor layout on reinstall.
    
    Signed-off-by: lvwvm <luwum@pm.me>

---
+++ .screenlayout/norma-vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS1 --mode 1366x768_60.04 --pos 277x0 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x768 --rotate normal --output VGA1 --off --output VIRTUAL1 --off
